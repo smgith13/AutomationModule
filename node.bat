167e24720c251204a5295164cdb7993b14a5790d
smgith13 <90877066+smgith13@users.noreply.github.com>
2022-03-13 15:24:31 -0400

    Add files via upload

---
+++ node.bat
@@ -0,0 +1,2 @@
+java -Dwebdriver.chrome.driver="C:\Users\aa\git\AutomationModule_updatedDemoQA\Automation.module\driver\chromedriver.exe" -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.0.68:8060/grid/register
+
